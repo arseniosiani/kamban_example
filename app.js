e49c9224498a06a32f0e6769b5864136df15e1e8
arseniosiani <arsenio.siani@gmail.com>
2017-08-23 22:41:18 +0200

    aggiunte un paio di librerie molto utili - express (per semplificare la creazione dell'applicazione) - hanldebar (template engine html)

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // questo è il file principale dell'applicazione
+
+//usiamo la libreria express
+var express = require('express');
+
+//usiamo la librerira express-handlebars
+var exphbs  = require('express-handlebars');
+
+// in app ci sarà la nostra applicazione
+var app = express();
+
+// configuriamo in modo che i file statici vengano presi dalla directory static_files
+app.use(express.static(__dirname + '/static_files'));
+
+// usiamo handlebars come template engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function (req, res) {
+    res.end("Per il momento questa app non fa niente!!")
+});
+
+app.listen(3000);
+console.log("Applicazione partita!! collegati a http://localhost:3000");
